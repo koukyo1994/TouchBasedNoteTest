e25c90490b87ba566b3c71a407633308dc0e23cc
koukyo1994 <arabiannight1994@yahoo.co.jp>
2019-12-31 14:51:23 +0100

    Initial Commit

---
+++ TouchBasedNoteTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchBasedNoteTest
+//
+//  Created by 荒居秀尚 on 31.12.19.
+//  Copyright © 2019 荒居秀尚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
