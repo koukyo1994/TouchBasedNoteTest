73275f4512bf47b8f792c79561fe512da0c1f41b
koukyo1994 <arabiannight1994@yahoo.co.jp>
2020-01-03 18:08:50 +0100

    点列の保存

--- TouchBasedNoteTest/CanvasView.swift
+++ TouchBasedNoteTest/CanvasView.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Foundation
 
 class CanvasView: UIImageView {
     private let baseSize: CGFloat = 5.0
@@ -17,19 +17,42 @@ class CanvasView: UIImageView {
     private var startPoint = CGPoint()
     private var touchPoint = CGPoint()
     
+    private var touchDate = Date()
+    private var touchEvents = [(interval: Double, point: CGPoint)]()
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first!
-        startPoint = touch.location(in: self)
+        let point = touch.location(in: self)
+        startPoint = point
+        
+        let date = Date()
+        let interval = touchDate.distance(to: date)
+        
+        touchDate = date
+        touchEvents.append((interval: Double(interval), point: point))
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first!
-        touchPoint = touch.location(in: self)
+        let point = touch.location(in: self)
+        touchPoint = point
+        
+        let date = Date()
+        let interval = touchDate.distance(to: date)
+        
+        touchDate = date
+        touchEvents.append((interval: Double(interval), point: point))
         
         path.move(to: startPoint)
         path.addLine(to: touchPoint)
         startPoint = touchPoint
         drawLine(touch: touch)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+        //    print(Date())
+        // })
     }
     
     func clear() {
